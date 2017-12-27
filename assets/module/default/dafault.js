a976dffa8d84868f13445c9ed6dd381a183874a0
wefashe <wefashe@qq.com>
2017-12-27 09:22:41 +0800

    修改样式

--- assets/module/default/dafault.js
+++ assets/module/default/dafault.js
@@ -1,5 +1,19 @@
-require(['jquery', 'knockout', 'bootstrap'],function($, ko) {
+require(['jquery', 'knockout', 'bootstrap'], function($, ko) {
 
 
+
+
+
+    function init() {
+        $(".pagination li").click(function() {
+
+            $(this).siblings('li').removeClass('active'); // 删除其他兄弟元素的样式
+
+            $(this).addClass('active'); // 添加当前元素的样式
+
+        });
+    };
+
+    init();
 
 });
